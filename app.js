307eda28c7fc82f3cb63172468fb1a3b6bc4cb60
RafaelMouraFrontend <rafael.mouraweb@gmail.com>
2019-03-11 20:52:21 +0000

    erro de validação

--- app.js
+++ app.js
@@ -6,8 +6,22 @@
   const admin = require('./routes/admin')
   const path = require('path')
   const mongoose = require('mongoose')
-
+  const session = require('express-session')
+  const flash = require('connect-flash')
 // Configs
+  // Session
+  app.use(session({
+    secret: 'cursodenone',
+    resave: true,
+    saveUninitialized: true
+  }))
+  app.use(flash())
+  // Middlaware
+  app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('succcess_msg')
+    res.locals.error_msg = req.flash('error_msg')
+    next()
+  })
   //body parser
     app.use(bodyParser.urlencoded({extended:true}))
     app.use(bodyParser.json())
@@ -25,6 +39,8 @@
     })
   //Public
     app.use(express.static(path.join(__dirname, "public")))
+
+
 // Routes
   app.use('/admin', admin)
 // Others
