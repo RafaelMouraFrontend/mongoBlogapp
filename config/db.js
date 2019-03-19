97d174bd011909f27bc0a03e150231a8e43b87e3
Rafael Moura <rafael.mouraweb@gmail.com>
2019-03-19 20:55:17 -0300

    ainda nos ajustes para deploy

--- config/db.js
+++ config/db.js
@@ -1,11 +1,8 @@
-
-
-
-
-
-
 if(process.env.NODE_ENV == 'production'){
-  module.exports = {mongoURI: 'mongodb+srv://moura:odi213web@cluster0-usmpv.mongodb.net/test?retryWrites=true'}
+  module.exports = require('./keys_dev')
 }else{
   module.exports = {mongoURI:'mongodb://localhost/blogapp'}
 }
+
+
+
--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,4 +1,8 @@
-module.exports = {
-  mongoURI: 'mongodb+srv://moura:odi123web@cluster0-usmpv.mongodb.net/test?retryWrites=true',
-  secretOrKey: 'SECRET'
-};
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://moura:odi123web>@cluster0-usmpv.mongodb.net/test?retryWrites=true";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
