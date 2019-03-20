d3a5a88dc69bf0a9231d870093f72b6615e3c34d
Rafael Moura <rafael.mouraweb@gmail.com>
2019-03-19 22:02:49 -0300

    erros deploy11

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV == "production"){
-  module.exports = {mongoURI: "mongodb+srv://moura:odi213web@cluster0-usmpv.mongodb.net/test?retryWrites=true"}
+  module.exports = {mongoURI: "mongodb+srv://moura:odi123web@cluster0-usmpv.mongodb.net/test?retryWrites=true"}
 }else{
   module.exports = {mongoURI: "mongodb://localhost/blogapp"}
 }
