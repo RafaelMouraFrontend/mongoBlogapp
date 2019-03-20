803acebb6539a98d4db381aab7d4f672dffade0f
Rafael Moura <rafael.mouraweb@gmail.com>
2019-03-19 23:00:52 -0300

    novo deployteste

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
-if(process.env.NODE_ENV == "production"){
-  module.exports = {mongoURI: "mongodb+srv://moura:odi123web@cluster0-usmpv.mongodb.net/test?retryWrites=true"}
+if(process.env.NODE_ENV == 'production'){
+  module.exports = {mongoURI: 'mongodb+srv://moura:odi123web@cluster0-usmpv.mongodb.net/test?retryWrites=true'}
 }else{
-  module.exports = {mongoURI: "mongodb://localhost/blogapp"}
+  module.exports = {mongoURI: 'mongodb://localhost/blogapp'}
 }
 
 
