bccaf217b6a853e0989f8c59e26e38ae45591bb3
Rafael Moura <rafael.mouraweb@gmail.com>
2019-03-19 23:46:40 -0300

    novo deploynovo clusteer

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV == 'production'){
-  module.exports = {mongoURI: 'mongodb://moura:odi123web@cluster0-shard-00-00-8zgqm.mongodb.net:27017,cluster0-shard-00-01-8zgqm.mongodb.net:27017,cluster0-shard-00-02-8zgqm.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin'}
+  module.exports = {mongoURI: 'mongodb+srv://moura:odi123web@cluster0-usmpv.mongodb.net/test?retryWrites=true'}
 }else{
   module.exports = {mongoURI: 'mongodb://localhost/blogapp'}
 }
