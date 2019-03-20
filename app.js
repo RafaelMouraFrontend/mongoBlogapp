d9483c6413160bb78a914ad32739502f24f20a10
Rafael Moura <rafael.mouraweb@gmail.com>
2019-03-19 21:56:30 -0300

    problemas deplo10

--- app.js
+++ app.js
@@ -44,7 +44,9 @@
     app.set('view engine', 'handlebars');
   //Mongoose
     mongoose.Promise = global.Promise
-    mongoose.connect(db.mongoURI).then(() => {
+    mongoose.connect(db.mongoURI, {
+    useNewUrlParser: true 
+  }).then(() => {
       console.log('Mongoose conected...');
     }).catch((err) => {
       console.log('erro ao se conectar: '+err);
