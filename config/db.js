80f8cf2f0ee73caec3d31d559e44de4c51a41f9c
Rafael Moura <rafael.mouraweb@gmail.com>
2019-03-19 23:11:40 -0300

    novo deploynovo cluster

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV == 'production'){
-  module.exports = {mongoURI: 'mongodb+srv://moura:odi123web@cluster0-usmpv.mongodb.net/test?retryWrites=true'}
+  module.exports = {mongoURI: 'mongodb://moura:odi123web@cluster0-shard-00-00-usmpv.mongodb.net:27017,cluster0-shard-00-01-usmpv.mongodb.net:27017,cluster0-shard-00-02-usmpv.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true'}
 }else{
   module.exports = {mongoURI: 'mongodb://localhost/blogapp'}
 }
